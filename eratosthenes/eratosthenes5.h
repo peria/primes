750a10cdecea508a095b8af59ad80457f02ec794
Hitoshi Yoshida <github@peria.jp>
2019-06-13 00:22:48 +0900

    Make ver 6 inherit ver 5

--- eratosthenes/eratosthenes5.h
+++ eratosthenes/eratosthenes5.h
@@ -6,7 +6,7 @@
 
 // Generate prime numbers up to x.
 // Use initialized list to avoid sieving with small primes. (<= 23)
-class Eratosthenes5 final : public Eratosthenes {
+class Eratosthenes5 : public Eratosthenes {
  public:
   void initialize() override;
   void generate(const int64 x) override;
@@ -14,15 +14,17 @@ class Eratosthenes5 final : public Eratosthenes {
   int64 count() override;
   int32 version() override { return 5; }
 
- private:
-  void initFlags(const int64 from_rem, const int64 to,
-                 const int64 offest, const int64 size);
-  void generateSmall(const uint64 offset, const int64 sqrt_to);
-  void generateCore(const int64 offset);
+ protected:
+  virtual void generateCore(const int64 offset);
 
   int64 num_primes_ = -1;
   std::vector<uint8> sflags_;
   std::vector<uint8> flags_;
   std::vector<uint64> indecies_;
   std::vector<uint8> initial_;
+
+ private:
+  void initFlags(const int64 from_rem, const int64 to,
+                 const int64 offest, const int64 size);
+  void generateSmall(const uint64 offset, const int64 sqrt_to);
 };
